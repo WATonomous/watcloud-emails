f6c4f276dc40f6b3956a91d9825710f0db49c900
Alex Boden <alex.boden@uwaterloo.ca>
2025-05-15 13:54:56 -0400

    Round mailing list email to integer for hours (#33)
    Resolves https://github.com/WATonomous/watcloud-emails/issues/31

--- emails/mailing-list.tsx
+++ emails/mailing-list.tsx
@@ -31,7 +31,7 @@ export function mailingListEmail(props: mailingListEmailProps) {
     return (
         <WATcloudEmail previewText={previewText}>
             <Text>Thanks for signing up for updates from {mailingList}</Text>
-            <Text>Please confirm your subscription by clicking the button below. This confirmation email will expire in {CODE_TTL_SEC / 60 / 60} hours. </Text>
+            <Text>Please confirm your subscription by clicking the button below. This confirmation email will expire in {Math.floor(CODE_TTL_SEC / 60 / 60)} hours. </Text>
             <Link className="confirmation-button" href={confirmationURL}>Confirm Subscription</Link>
             <Text>If the button above does not work, please copy and paste the following URL into your browser</Text>
             <pre>{confirmationURL}</pre>
