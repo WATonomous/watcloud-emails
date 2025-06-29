a61a20d404fe9ca2e927e1b9b3612ea181468e4b
Ben Zhang <benzhangniu@gmail.com>
2025-06-28 22:52:30 -0700

    Include all dependencies in the final bundle

--- apps/cli/tsup.config.ts
+++ apps/cli/tsup.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'tsup';
 export default defineConfig((options) => ({
     entry: ['src/index.ts'],
     format: ['cjs'],
-    dts: true,
-    sourcemap: true,
+    // Don't need QoL features when building the final bundle
+    dts: Boolean(options.watch),
+    sourcemap: Boolean(options.watch),
     minify: !options.watch,
-    // Include internal dependencies in the bundle
-    noExternal: [
-        /@repo\/.*/,
-        /@watonomous\/watcloud-email-.*/,
-    ],
+    // Include all dependencies in the final bundle
+    noExternal: options.watch ? [] : [ /.*/ ],
+    // Clean the output directory before building when building the final bundle
+    clean: !options.watch,
 }));
