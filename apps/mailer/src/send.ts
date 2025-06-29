b1b1a828eeaae02f291964d809c725d975196926
Ben Zhang <benzhangniu@gmail.com>
2025-06-29 03:24:41 -0700

    Implement sending email with nodemailer

--- apps/mailer/src/send.ts
+++ apps/mailer/src/send.ts
@@ -1,6 +1,42 @@
-import nodemailer from "nodemailer";
 import { FissionContext, FissionCallback } from "./utils";
+import { getTransporter } from "./smtp-utils";
 
 module.exports = async function ({request, response}: FissionContext, cb: FissionCallback) {
-    response.status(200).json({});
+    if (request.method !== "POST") {
+        response.status(405).json({
+            error: "Only POST is supported",
+        });
+        return;
+    }
+
+    if (!request.body?.data) {
+        response.status(400).json({
+            error: "Missing request.body.data",
+        });
+        return;
+    }
+
+    const transporter = await getTransporter();
+
+    try {
+        const result = await transporter.sendMail({
+            from: request.body.data.from,
+            to: request.body.data.to,
+            replyTo: request.body.data.reply_to,
+            subject: request.body.data.subject,
+            text: request.body.data.text,
+            html: request.body.data.html,
+        })
+
+        response.status(200).json({
+            "transporter.isIdle": transporter.isIdle(),
+            "result": result,
+        });
+    } catch (e) {
+        console.error(e);
+        response.status(500).json({
+            error: "Failed to send email: " + e,
+        });
+        return;
+    }
 }
